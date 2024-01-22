d8bc17e96c837cf36f4229da6b70d85701d1d22d
benben175 <benben1752002@gmail.com>
2024-01-22 17:32:09 +0000

    Implement menu for entering room code

---
+++ Assets/RoomJoining.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class RoomJoining : MonoBehaviour
+{
+
+    public TMP_InputField roomCodeInput;
+
+    public void Start() {
+        //  Potentially need some initialisation here for P2P.
+    }
+
+    public void JoinRoom() {
+        string roomCode = roomCodeInput.text;
+        Debug.Log("Joining room: " + roomCode);
+
+        if (foundRoom(roomCode)) {
+            //  Join the room.
+            SceneManager.LoadScene("Gameplay");
+        } else {
+            CreateRoom(roomCode);
+        }
+    }
+
+    private void CreateRoom(string roomCode) {
+        //  Create the room.
+    }
+
+    private bool foundRoom(string roomCode) {
+        return roomCode == "123456";
+    }
+}
