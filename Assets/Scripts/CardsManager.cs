aa18547e66dad7320298723f3a9e1b4ac59880b2
hsh121 <huan.ham21@imperial.ac.uk>
2024-01-23 12:20:05 +0000

    Basic card generating

--- Assets/Scripts/CardsManager.cs
+++ Assets/Scripts/CardsManager.cs
@@ -1,22 +1,68 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class CardsManager : MonoBehaviour
 {
 
-    public Dictionary<String, Texture2D> cardImgs = new Dictionary<string, Texture2D>();
+    private Dictionary<string, (Sprite img, string stats)> cards = new Dictionary<string, (Sprite, string)>();
+
+    public GameObject cardPrefab;
+
+    public List<string> cardNames = new List<string>();
+    public List<Sprite> cardImgs = new List<Sprite>();
+    public List<string> cardStats = new List<string>();
+
+    public int cardsToCreate = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cardNames.Count != cardImgs.Count 
+            || cardNames.Count != cardStats.Count 
+            || cardImgs.Count != cardStats.Count) {
+            Debug.LogWarning("Cards Manager: Card Names, Card Images and Card Stats provided do not have the same amount. Please check these fields.");
+        }
+
+        int i;
+        for (i = 0; i < Math.Min(cardNames.Count, cardImgs.Count); i++) {
+            cards.Add(cardNames[i], (cardImgs[i], cardStats[i]));
+            Debug.Log($"Cards Manager: Card initialised - {cardNames[i]}");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (cardsToCreate > 0) {
+            createCard("baseAtk");
+            cardsToCreate -= 1;
+        }
+    }
+
+
+    private void createCard(string cardName) {
+        if (cards.ContainsKey(cardName)) {
+            // Render card image
+            GameObject cardImgObj = cardPrefab.transform.GetChild(0).gameObject;
+            Sprite cardImg = cards[cardName].img;
+            SpriteRenderer cardImgRenderer = cardImgObj.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+            cardImgRenderer.sprite = cardImg;
+
+            // Render card stats
+            GameObject cardStatsObj = cardPrefab.transform.GetChild(1).gameObject;
+            string cardStat = cards[cardName].stats;
+            TextMeshPro textComp = cardStatsObj.GetComponent(typeof(TextMeshPro)) as TextMeshPro;
+            textComp.text = cardStat;
+
+            Instantiate(cardPrefab);
+        } else {
+            Debug.Log($"Cards Manager: Card image not found - {cardName}. Cannot create card.");
+        }
     }
 }
