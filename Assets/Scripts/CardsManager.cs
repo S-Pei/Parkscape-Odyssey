34aefeb96d0e804cf83ca5a63efd40c828edc727
hsh121 <huan.ham21@imperial.ac.uk>
2024-01-23 11:24:31 +0000

    Initialise CardsManager script

---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsManager : MonoBehaviour
+{
+
+    public Dictionary<String, Texture2D> cardImgs = new Dictionary<string, Texture2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
