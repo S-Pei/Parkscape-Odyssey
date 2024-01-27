fba19c6a571778268d3a4f7e29817a503991ed00
Siu Pei Ooi <spo21@ic.ac.uk>
2024-01-27 21:16:04 +0000

    fixed p2p connection, able to send string

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -15,20 +15,29 @@ public class TestScript : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI endpointListStr;
+
+    [SerializeField]
+    private TextMeshProUGUI dataStr;
     
     private AndroidJavaObject p2pObj;
 
     void Start() {
         Debug.Log("runs here");
-        p2pObj = new AndroidJavaObject("com.example.p2pnetwork.ManualConnect");
+        // p2pObj = new AndroidJavaObject("com.example.p2pnetwork.ManualConnect");
+        AndroidJavaClass unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        p2pObj = unityClass.GetStatic<AndroidJavaObject>("currentActivity");     
 
-        int javaInt = p2pObj.Call<int>("getInt");
+        // int javaInt = unityActivity.Call<int>("getInt");
 
         // string javaStr = p2pObj.Call<string>("getString");
         // testInt.text = javaInt.ToString();
         // testString.text = javaStr;
         name.text = p2pObj.Call<string>("getDeviceName");
         
+    }
+
+    void Update() {
+        dataStr.text = getData();
     }
 
     // to be called from UI
@@ -45,4 +54,18 @@ public class TestScript : MonoBehaviour
     public void getDiscoveredEndpointList() {
         endpointListStr.text = p2pObj.Call<string>("getDiscoveredEndpointList");
     }
+
+    // sends "hello world" hardcoded string (called from UI)
+    public void sendString() {
+        string str = "Hello World";
+        p2pObj.Call("sendString", str);
+    }
+
+    // gets data sent from other devices
+    public string getData() {
+        string data = p2pObj.Call<string>("getData");
+        return data;
+    }
+
+
 } 
