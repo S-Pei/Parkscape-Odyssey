09537408494734c5eec6a768ceea7826d6b98bb0
Benjamin Teoh <bt221@ic.ac.uk>
2024-01-29 23:53:22 +0000

    Added role designs and change role image depending on role

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,17 @@ public class GameManager : MonoBehaviour
 {
     private GameInterfaceManager gameInterfaceManager;
 
+    [SerializeField]
+    private string role;
+
     private List<string> playerCards = new List<string> { "baseAtk", "baseAtk", "baseDef", "baseDef", "baseAtk", "baseDef" };
 
     // Start is called before the first frame update
     void Start() {
         gameInterfaceManager = (GameInterfaceManager) GetComponent(typeof(GameInterfaceManager));
+
+        // Set image according to role
+        gameInterfaceManager.setRole(role);
     }
 
     // Update is called once per frame
