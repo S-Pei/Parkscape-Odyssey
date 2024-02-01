883c0a14b361f2c720d92d54b0857a81e7ed4da0
hsh121 <huan.ham21@imperial.ac.uk>
2024-02-01 19:30:50 +0000

    Initialise Monster class

---
+++ Assets/Scripts/CombatUtils/Monster.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+class Monster {
+
+  public readonly MonsterName name;
+
+  public readonly Sprite img;
+
+  public readonly int health;
+
+  public readonly int baseDamage;
+
+  // public readonly 
+}
