bd0de17f627b2ca01f6f9b3c177d71bfb4cd5490
bt221 <bt221@ic.ac.uk>
2024-02-02 03:15:17 +0000

    Remove dependency on order of roles

--- Assets/Scripts/GameInterfaceManager.cs
+++ Assets/Scripts/GameInterfaceManager.cs
@@ -1,11 +1,11 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameInterfaceManager : MonoBehaviour
 {
-    private GameState GameState = GameState.Instance;
+    private readonly GameState GameState = GameState.Instance;
     private GameObject inventoryObject;
 
     [SerializeField]
@@ -19,53 +19,43 @@ public class GameInterfaceManager : MonoBehaviour
 
     [SerializeField]
     private List<Sprite> playerIcons;
-    private Dictionary<string, int> roles = new Dictionary<string, int> { 
-        { "Rogue",  0 },
-        { "Mage",  1 },
-        { "Faerie",  2 },
-        { "Cleric",  3 },
-        { "Scout",  4 },
-        { "Warrior",  5 }, };
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // Open with actual inventory stored in GameManager
     public void OpenInventory(List<CardName> cards) {
         inventoryObject = Instantiate(inventoryPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        InventoryController inventoryController = (InventoryController) inventoryObject.GetComponent<InventoryController>();
+        InventoryController inventoryController = inventoryObject.GetComponent<InventoryController>();
         inventoryController.inventoryCards = cards;
     }
 
     public void CloseInventory() {
-        InventoryUIManager inventoryUIManager = (InventoryUIManager) inventoryObject.GetComponent<InventoryUIManager>();
+        InventoryUIManager inventoryUIManager = inventoryObject.GetComponent<InventoryUIManager>();
         inventoryUIManager.DestroySelf();
     }
 
     public void SetUpInterface() {
         // Set up player icon.
-        ((Image) playerIcon.GetComponent(typeof(Image))).sprite = playerIcons[roles[GameState.MyPlayer.Role]];
+        ((Image) playerIcon.GetComponent(typeof(Image))).sprite = GetIcon(GameState.MyPlayer.Role);
     }
 
     // Opens the player view with the player's role informationa and stats
     public void OpenPlayerView() {
         GameObject playerView = Instantiate(playerViewPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         playerView.SetActive(false);
-        PlayerViewManager playerViewManager = (PlayerViewManager) playerView.GetComponent<PlayerViewManager>();
+        PlayerViewManager playerViewManager = playerView.GetComponent<PlayerViewManager>();
 
         // Role information
         playerViewManager.SetPlayer(GameState.MyPlayer);
-        playerViewManager.SetPlayerIcon(playerIcons[roles[GameState.MyPlayer.Role]]);
+        playerViewManager.SetPlayerIcon(GetIcon(GameState.MyPlayer.Role));
 
         playerView.SetActive(true);
+    }
+
+    private Sprite GetIcon(string role) {
+        foreach (Sprite icon in playerIcons) {
+            if (icon.name.Contains(role)) {
+                return icon;
+            }
+        }
+        throw new Exception("Role Icon not found");
     }
 }
