6817528829f2bbf09ec8ecd0c51315abb4186a14
hsh121 <huan.ham21@imperial.ac.uk>
2024-02-07 02:42:25 +0000

    Initialise base network utils in EncounterController

--- Assets/Scripts/EncounterController.cs
+++ Assets/Scripts/EncounterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,67 @@ using Newtonsoft.Json;
 
 public class EncounterController : MonoBehaviour
 {
+    // p2p network
+    private NetworkUtils network;
+    private bool AcceptMessages = false;
+    private readonly float baseFreq = 0.1f;
+    private int msgFreq = 0;
+    private int msgFreqCounter = 0;
 
+    
+    void Awake() {
+        // Setup p2p network
+        msgFreq = GameState.Instance.maxPlayerCount;
+        network = NetworkManager.Instance.NetworkUtils;
+        InvokeRepeating("HandleMessages", 0.0f, baseFreq);
+    }
+
+    // ------------------------------ P2P NETWORK ------------------------------
+    private void HandleMessages() {
+        // Set up callback for handling incoming messages.
+        Func<Message, CallbackStatus> callback = (Message msg) => {
+            return HandleMessage(msg);
+        };
+        network.onReceive(callback);
+
+        // Every msgFreq seconds, send messages.
+        if (msgFreqCounter >= msgFreq) {
+            SendMessages();
+            msgFreqCounter = 0;
+        } else {
+            msgFreqCounter++;
+        }
+    }
+
+    private CallbackStatus HandleMessage(Message message) {
+        // Ignore if not an encounter message.
+        if (message.messageInfo.messageType != MessageType.ENCOUNTERMESSAGE)
+            return CallbackStatus.NOT_PROCESSED;
+
+        // Ignore if no longer accepting messages.
+        if (!AcceptMessages)
+            return CallbackStatus.DORMANT;
+
+        EncounterMessage encounterMessage = (EncounterMessage) message.messageInfo;
+        switch (encounterMessage.Type) {
+            case EncounterMessageType.FOUND_ENCOUNTER:
+                // openEncounterLobbyPopup();
+                break;
+        }
+
+        return CallbackStatus.PROCESSED;
+    }
+
+    private void SendMessages() {
+        if (network == null)
+            return;
+
+        if (!AcceptMessages) {
+            return;
+        }
+    }
 }
+
 
 public enum EncounterMessageType {
     FOUND_ENCOUNTER,
