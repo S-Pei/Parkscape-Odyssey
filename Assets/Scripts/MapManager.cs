1d3e6fd1af4bcb7794ae9aa2b01ce2df256cc16a
spo21 <spo21@ic.ac.uk>
2024-02-21 11:08:39 +0000

    log

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -177,6 +177,7 @@ public class MapManager : MonoBehaviour
     public void AddMediumEncounterPins() {
         foreach (var entry in GameState.Instance.mediumEncounterLocations) {
             Debug.Log("Adding pin for " + entry.Key);
+            Debug.Log(encounterController);
             encounterController.CreateMonsterSpawn(entry.Value);
         }
     }
