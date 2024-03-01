5e96f196575dac0e26a78e487435c7c56d7c8013
hsh121 <huan.ham21@imperial.ac.uk>
2024-03-01 11:34:37 +0000

    Comment out unused functions

--- Assets/Scripts/ARManager.cs
+++ Assets/Scripts/ARManager.cs
@@ -36,12 +36,10 @@ public class ARManager : MonoBehaviour
     public void StartAR() {
         Debug.Log("Starting AR session.");
         arCamera.SetActive(true);
-        // SpawnAllLocations();
     }
 
     public void StopAR() {
         Debug.Log("Stopping AR session.");
-        // DeSpawnAllLocations();
         arCamera.SetActive(false);
     }
 
@@ -50,20 +48,22 @@ public class ARManager : MonoBehaviour
         return ScreenCapture.CaptureScreenshotAsTexture();
     }
 
-    public void SpawnAllLocations() {
-        foreach (GameObject arLocation in arSpawnLocations) {
-            GameObject spawnedARLocation = Instantiate(arLocation, xrOrigin.transform);
-            spawnedARLocation.transform.SetParent(xrOrigin.transform);
-        }
-    }
+    // NOT USED FOR NOW
+    // public void SpawnAllLocations() {
+    //     foreach (GameObject arLocation in arSpawnLocations) {
+    //         GameObject spawnedARLocation = Instantiate(arLocation, xrOrigin.transform);
+    //         spawnedARLocation.transform.SetParent(xrOrigin.transform);
+    //     }
+    // }
 
-    public void DeSpawnAllLocations() {
-        var i = 0;
-        foreach (Transform child in xrOrigin.transform) {
-            if (i != 0) {
-                Destroy(child.gameObject);
-            }
-            i++;
-        }
-    }
+    // NOT USED FOR NOW
+    // public void DeSpawnAllLocations() {
+    //     var i = 0;
+    //     foreach (Transform child in xrOrigin.transform) {
+    //         if (i != 0) {
+    //             Destroy(child.gameObject);
+    //         }
+    //         i++;
+    //     }
+    // }
 }
