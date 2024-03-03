4f6456a2fa422aeeb65d102170b07d6d2695f42a
bt221 <bt221@ic.ac.uk>
2024-03-03 03:37:12 +0000

    Respawn object if fall off world

--- Assets/Scripts/ARObjectSpawner.cs
+++ Assets/Scripts/ARObjectSpawner.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ARObjectSpawner : MonoBehaviour {
@@ -6,6 +8,13 @@ public class ARObjectSpawner : MonoBehaviour {
 
     [SerializeField]
     private Camera arCamera;
+
+    [SerializeField]
+    private bool gravity = true;
+
+    private Dictionary<GameObject, Action> trackObjects = new();
+
+    private const float threshold = -500;
 
     void Start() {
         if (arObjectPrefab == null) {
@@ -26,14 +35,32 @@ public class ARObjectSpawner : MonoBehaviour {
         //     var obj = SpawnARObject();
         //     Debug.Log("Spawned AR object: " + obj);
         // }
+
+        // Track if the object has settled on the ground
+        if (!gravity)
+            return;
+
+        foreach (var obj in trackObjects) {
+            if (obj.Key.transform.position.y < threshold) {
+                trackObjects.Remove(obj.Key);
+                Destroy(obj.Key);
+                // Spawn new object
+                obj.Value();
+            }
+        }   
     }
 
     public GameObject SpawnARObject(float distance = 2.0f) {
-        //spawn in front of at the camera
-        // will fall to the ground
+        //spawn in front of at the camera with some random degree deviation.
         var pos = arCamera.transform.position;
-        var forw = arCamera.transform.forward;
+        var forw = arCamera.transform.forward.normalized;
         var obj = Instantiate(arObjectPrefab, pos + (forw * distance), Quaternion.identity);
+
+        // Track if the object has settled on the ground
+        if (!gravity)
+            return obj;
+        
+        trackObjects.Add(obj, () => SpawnARObject(distance));
         return obj;
     }
 }
