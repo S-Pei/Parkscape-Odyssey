2c714db7aeb4b0b5a8d9633b67460d51b036a92e
hsh121 <huan.ham21@imperial.ac.uk>
2024-03-04 21:28:54 +0000

    Create GetDetectGroundPoints func

--- Assets/Scripts/EncounterObjectManager.cs
+++ Assets/Scripts/EncounterObjectManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EncounterObjectManager : MonoBehaviour
 {
+    private int COL_DETECT_POINTS_NUM = 4;
+    private int ROW_DETECT_POINTS_NUM = 3;
+
+
     void Update() {
         if (Input.touches.Length > 0) {
             Touch touch = Input.GetTouch(0);
@@ -17,5 +21,19 @@ public class EncounterObjectManager : MonoBehaviour
                 }
             }
         }
+    }
+
+    
+
+    // Get all the points in the grid to detect the ground
+    private List<(int, int)> GetDetectGroundPoints() {
+        List<(int, int)> detectPoints = new List<(int, int)>();
+
+        for (int i = 1; i <= ROW_DETECT_POINTS_NUM; i++) {
+            for (int j = 1; j <= COL_DETECT_POINTS_NUM; j++) {
+                detectPoints.Add((Screen.width / (ROW_DETECT_POINTS_NUM + 1) * i, Screen.height / (COL_DETECT_POINTS_NUM + 1) * j));
+            }
+        }
+        return detectPoints;
     }
 }
