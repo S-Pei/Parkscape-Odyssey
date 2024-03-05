34f7bc9bdf9a7608f7f9518ceee89e7917c6cc21
hsh121 <huan.ham21@imperial.ac.uk>
2024-03-05 01:38:16 +0000

    Remove unused imports

--- Assets/Scripts/EncounterObjectManager.cs
+++ Assets/Scripts/EncounterObjectManager.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Geospatial;
-using Niantic.Lightship.AR.Semantics;
 using UnityEngine;
 
 public class EncounterObjectManager : MonoBehaviour
