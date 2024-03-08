429437f3690109780e3b6453f4fe530ffc4ba286
Ian Sung <ian.sung21@imperial.ac.uk>
2024-03-08 02:42:02 +0000

    Reduce log output in pipeline building

--- ci/build.sh
+++ ci/build.sh
@@ -18,7 +18,7 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -customBuildName $BUILD_NAME \
   -customBuildPath $BUILD_PATH \
   -executeMethod BuildCommand.PerformBuild \
-  -logFile /dev/stdout
+  -logFile $UNITY_DIR/output/log
 
 UNITY_EXIT_CODE=$?
 
